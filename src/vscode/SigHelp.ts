9ca488c8262a849c33d68ca8468750d6bdd1d2dc
Rich Heller <rellehr@gmail.com>
2020-11-06 22:27:44 -0600

    Fix signature help logic

--- src/vscode/SigHelp.ts
+++ src/vscode/SigHelp.ts
@@ -1,5 +1,5 @@
-import { Position, SignatureHelp, TextDocument, SignatureInformation, ParameterInformation } from 'vscode'
-import { Expr, exprToString, findExpr, SExpr, Lexer, Parser, posInExpr } from '../lisp'
+import { ParameterInformation, Position, SignatureHelp, SignatureInformation, TextDocument } from 'vscode'
+import { Expr, exprToString, findExpr, Lexer, Parser, posInExpr, SExpr } from '../lisp'
 import { Repl } from './repl'
 import { getDocumentExprs } from './Utils'
 
@@ -37,36 +37,68 @@ async function getFuncHelp(repl: Repl, label: string, args: Expr[], pos: Positio
     return exprToHelp(desc, descExpr, args, pos)
 }
 
-function exprToHelp(desc: string, expr: Expr, args: Expr[], pos: Position): SignatureHelp {
+function exprToHelp(desc: string, descExpr: Expr, args: Expr[], pos: Position): SignatureHelp {
     const help = new SignatureHelp()
 
-    if (!(expr instanceof SExpr)) {
+    if (!(descExpr instanceof SExpr)) {
         return help
     }
 
-    const sigInfo = new SignatureInformation(desc)
-    sigInfo.activeParameter = undefined
-
-    for (let ndx = 1; ndx < expr.parts.length; ndx += 1) {
-        const part = expr.parts[ndx]
-        const arg = ndx <= args.length ? args[ndx - 1] : undefined
-        const range: [number, number] = [part.start.character, part.end.character]
-
-        if ((sigInfo.activeParameter === undefined && arg === undefined) || (arg !== undefined && posInExpr(arg, pos))) {
-            sigInfo.activeParameter = ndx - 1
-        }
-
-        sigInfo.parameters.push(new ParameterInformation(range))
-    }
-
-    if (sigInfo.activeParameter === undefined) {
-        sigInfo.activeParameter = expr.parts.length
-    }
+    const sigInfo = buildSigInfo(desc, descExpr.parts.slice(1), args, pos)
 
     help.signatures.push(sigInfo)
     help.activeSignature = 0
 
     return help
+}
+
+function buildSigInfo(desc: string, descParts: Expr[], args: Expr[], pos: Position): SignatureInformation {
+    const sigInfo = new SignatureInformation(desc)
+    let foundOptional: boolean = false
+    let restNdx: number | undefined = undefined
+
+    descParts.forEach((part, ndx) => {
+        if (foundOptional) {
+            ndx -= 1
+        }
+
+        const arg = ndx <= args.length ? args[ndx] : undefined
+        const range: [number, number] = [part.start.character, part.end.character]
+
+        if (isAmpOption(part, '&optional')) {
+            foundOptional = true
+            return
+        } else if (isAmpOption(part, '&rest')) {
+            restNdx = ndx
+            return
+        } else if ((sigInfo.activeParameter === undefined && arg === undefined) || (arg !== undefined && posInExpr(arg, pos))) {
+            sigInfo.activeParameter = restNdx !== undefined ? restNdx : ndx
+        }
+
+        sigInfo.parameters.push(new ParameterInformation(range))
+    })
+
+    if (sigInfo.activeParameter === undefined) {
+        if (restNdx !== undefined) {
+            sigInfo.activeParameter = restNdx
+        } else if (foundOptional) {
+            sigInfo.activeParameter = descParts.length - 1
+        } else {
+            sigInfo.activeParameter = descParts.length
+        }
+    }
+
+    return sigInfo
+}
+
+function isAmpOption(expr: Expr, target: string): boolean {
+    const str = exprToString(expr)
+
+    if (str === undefined) {
+        return false
+    }
+
+    return str.toLowerCase() === target
 }
 
 function parseFuncDesc(desc: string): Expr[] {
