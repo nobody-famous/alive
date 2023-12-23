106e066506f2e63925970316a9f34c615281179a
Rich Heller <rellehr@gmail.com>
2023-12-22 20:31:03 -0600

    Work on type guard tests

--- src/vscode/Guards.ts
+++ src/vscode/Guards.ts
@@ -1,40 +1,24 @@
 import { Position } from 'vscode'
 import { RestartInfo, SourceLocation } from './Types'
 
+export function isString(data: unknown): data is string {
+    return typeof data === 'string'
+}
+
+export function isFiniteNumber(data: unknown): data is number {
+    return Number.isFinite(data)
+}
+
+export function isObject(data: unknown): data is Record<string, unknown> {
+    return typeof data === 'object' && data !== null && Object.keys(data).every(isString)
+}
+
 export function isPosition(item: unknown): item is Position {
-    if (typeof item !== 'object' || item === undefined) {
-        return false
-    }
-
-    const itemObj = item as { [index: string]: unknown }
-
-    if (typeof itemObj.line !== 'number' || typeof itemObj.character !== 'number') {
-        return false
-    }
-
-    return true
+    return isObject(item) && isFiniteNumber(item.line) && isFiniteNumber(item.character)
 }
 
 export function isSourceLocation(item: unknown): item is SourceLocation {
-    if (typeof item !== 'object' || item === undefined) {
-        return false
-    }
-
-    const itemObj = item as { [index: string]: unknown }
-
-    if (typeof itemObj.function !== 'string') {
-        return false
-    }
-
-    if (itemObj.file !== null && typeof itemObj.file !== 'string') {
-        return false
-    }
-
-    if (itemObj.position !== null && !isPosition(itemObj.position)) {
-        return false
-    }
-
-    return true
+    return isObject(item) && isString(item.function) && isString(item.file) && isPosition(item.position)
 }
 
 export function isStackTrace(item: unknown): item is SourceLocation[] {
--- src/vscode/__test__/Guards.spec.ts
+++ src/vscode/__test__/Guards.spec.ts
@@ -1,12 +1,41 @@
 import { strictEqual } from 'assert'
-import { isPosition } from '../Guards'
+import { isFiniteNumber, isObject, isPosition, isSourceLocation, isString } from '../Guards'
 
 describe('Type guard tests', () => {
+    it('isString', () => {
+        strictEqual(true, isString(''))
+        strictEqual(true, isString('foo'))
+        strictEqual(false, isString(5))
+        strictEqual(false, isString({ foo: 'bar' }))
+    })
+
+    it('isFiniteNumber', () => {
+        strictEqual(true, isFiniteNumber(5))
+        strictEqual(true, isFiniteNumber(1e9))
+        strictEqual(false, isFiniteNumber(NaN))
+        strictEqual(false, isFiniteNumber(1e1000))
+        strictEqual(false, isFiniteNumber('foo'))
+    })
+
+    it('isObject', () => {
+        strictEqual(true, isObject({}))
+        strictEqual(true, isObject({ foo: 'bar' }))
+        strictEqual(false, isObject(NaN))
+        strictEqual(false, isObject(1e1000))
+        strictEqual(false, isObject('foo'))
+    })
+
     it('isPosition', () => {
+        strictEqual(true, isPosition({ line: 5, character: 10 }))
+        strictEqual(false, isPosition({ line: 'foo', character: 10 }))
+        strictEqual(false, isPosition({ line: 5, character: 'bar' }))
         strictEqual(false, isPosition(5))
     })
 
     it('isSourceLocation', () => {
-        strictEqual(false, true)
+        strictEqual(true, isSourceLocation({ function: 'foo', file: 'bar', position: { line: 5, character: 10 } }))
+        strictEqual(false, isSourceLocation({ function: 'foo', file: 'bar' }))
+        strictEqual(false, isSourceLocation({ function: 'foo', position: { line: 5, character: 10 } }))
+        strictEqual(false, isSourceLocation({ file: 'bar', position: { line: 5, character: 10 } }))
     })
 })
