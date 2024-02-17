a5e05c531d6d17a513b226fd0f597b1e0a29510b
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2024-02-17 10:26:37 -0600

    Config coverage tests (#187)

---
+++ src/__test__/config.spec.ts
@@ -0,0 +1,27 @@
+import { AliveConfig, readAliveConfig } from '../config'
+
+const vscodeMock = jest.requireMock('vscode')
+jest.mock('vscode')
+
+describe('Config Tests', () => {
+    describe('readAliveConfig', () => {
+        const runTest = (fakeCfg: unknown, validate: (cfg: AliveConfig) => void) => {
+            vscodeMock.workspace.getConfiguration.mockImplementationOnce(() => fakeCfg)
+
+            validate(readAliveConfig())
+        }
+
+        it('Basic settings', () => {
+            runTest({}, (cfg) => expect(cfg.enableDiagnostics).toBe(true))
+            runTest({ enableDiagnostics: false }, (cfg) => expect(cfg.enableDiagnostics).toBe(false))
+        })
+
+        it('LSP settings', () => {
+            runTest({ lsp: { install: { path: 'foo' } } }, (cfg) => expect(cfg.lsp.install.path).toBe('foo'))
+            runTest({ lsp: { downloadUrl: 'bar' } }, (cfg) => expect(cfg.lsp.downloadUrl).toBe('bar'))
+            runTest({ lsp: { remote: { host: 'baz' } } }, (cfg) => expect(cfg.lsp.remote.host).toBe('baz'))
+            runTest({ lsp: { remote: { port: 1234 } } }, (cfg) => expect(cfg.lsp.remote.port).toBe(1234))
+            runTest({ lsp: { startCommand: ['bar'] } }, (cfg) => expect(cfg.lsp.startCommand).toMatchObject(['bar']))
+        })
+    })
+})
