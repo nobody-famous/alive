22091ee22cff984d87dc89e08f22e181edd9730a
Rich Heller <rellehr@gmail.com>
2024-03-19 22:29:00 -0500

    Remove mocks for guards and utils

--- src/vscode/backend/__test__/LspProcess.spec.ts
+++ src/vscode/backend/__test__/LspProcess.spec.ts
@@ -3,9 +3,6 @@ import { downloadLspServer } from '../LspProcess'
 const vscodeMock = jest.requireMock('vscode')
 jest.mock('vscode')
 
-const guardsMock = jest.requireMock('../../Guards')
-jest.mock('../../Guards')
-
 jest.mock('axios')
 
 describe('LspProcess tests', () => {
@@ -20,7 +17,6 @@ describe('LspProcess tests', () => {
         it('Base path', async () => {
             vscodeMock.workspace.getConfiguration.mockReturnValueOnce({ get: () => undefined })
             vscodeMock.extensions.getExtension.mockReturnValueOnce(undefined)
-            guardsMock.isString.mockReturnValueOnce(false)
 
             const resp = await downloadLspServer()
 
@@ -31,8 +27,6 @@ describe('LspProcess tests', () => {
             vscodeMock.workspace.getConfiguration.mockReturnValueOnce({ get: () => undefined })
             vscodeMock.workspace.getConfiguration.mockReturnValueOnce({ lsp: { downloadUrl: 'url' } })
             vscodeMock.extensions.getExtension.mockReturnValueOnce({ extensionPath: '/some/path' })
-            guardsMock.isString.mockReturnValueOnce(false) // config path
-            guardsMock.isString.mockReturnValueOnce(false) // url
 
             const resp = await downloadLspServer()
 
