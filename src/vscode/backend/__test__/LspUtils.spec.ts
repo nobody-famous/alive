84565eaf46142766754a310c760509dfd19b1289
Rich Heller <rellehr@gmail.com>
2024-04-07 13:43:03 -0500

    LspUtils coverage tests

--- src/vscode/backend/__test__/LspUtils.spec.ts
+++ src/vscode/backend/__test__/LspUtils.spec.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import { createPath, doesPathExist, getInstalledVersion, getLatestVersion, pullLatestVersion } from '../LspUtils'
+import { GitHubVersion } from '../../Types'
 
 const axiosMock = jest.requireMock('axios')
 jest.mock('axios')
@@ -13,32 +14,67 @@ jest.mock('../ZipUtils')
 describe('LspUtils tests', () => {
     describe('getLatestVersion', () => {
         it('Sort versions', async () => {
-            axiosMock.mockReturnValueOnce({
-                data: [
-                    {
-                        created_at: '1/1/2000',
-                        name: 'first',
-                        tag_name: 'first',
-                        zipball_url: 'zip_url',
-                    },
-                    {
-                        created_at: '1/1/2010',
-                        name: 'second',
-                        tag_name: 'second',
-                        zipball_url: 'zip_url',
-                    },
-                ],
-            })
+            const createFakeData = (dates: string[]) => {
+                const data: GitHubVersion[] = []
+
+                dates.forEach((date, index) =>
+                    data.push({
+                        created_at: date,
+                        name: `name ${index}`,
+                        tag_name: `tag_name ${index}`,
+                        zipball_url: `zipball_url ${index}`,
+                    })
+                )
+
+                return data
+            }
+
+            axiosMock.mockReturnValueOnce({ data: createFakeData(['1/1/2000', '1/1/2010']) })
             expect(await getLatestVersion('/some/url')).toMatchObject({
                 createdAt: Date.parse('1/1/2010'),
-                name: 'second',
-                tagName: 'second',
-                zipballUrl: 'zip_url',
+                name: 'name 1',
+                tagName: 'tag_name 1',
+                zipballUrl: 'zipball_url 1',
+            })
+
+            axiosMock.mockReturnValueOnce({ data: createFakeData(['1/1/2010', '1/1/2000']) })
+            expect(await getLatestVersion('/some/url')).toMatchObject({
+                createdAt: Date.parse('1/1/2010'),
+                name: 'name 0',
+                tagName: 'tag_name 0',
+                zipballUrl: 'zipball_url 0',
+            })
+
+            axiosMock.mockReturnValueOnce({ data: createFakeData(['1/1/2010', '1/1/2010']) })
+            expect(await getLatestVersion('/some/url')).toMatchObject({
+                createdAt: Date.parse('1/1/2010'),
+                name: 'name 0',
+                tagName: 'tag_name 0',
+                zipballUrl: 'zipball_url 0',
+            })
+
+            axiosMock.mockReturnValueOnce({ data: createFakeData(['Invalid date']) })
+            expect(await getLatestVersion('/some/url')).toMatchObject({
+                createdAt: 0,
+                name: 'name 0',
+                tagName: 'tag_name 0',
+                zipballUrl: 'zipball_url 0',
+            })
+
+            axiosMock.mockReturnValueOnce({ data: createFakeData(['Invalid date', 'Invalid date']) })
+            expect(await getLatestVersion('/some/url')).toMatchObject({
+                createdAt: 0,
+                name: 'name 0',
+                tagName: 'tag_name 0',
+                zipballUrl: 'zipball_url 0',
             })
         })
 
         it('Invalid data', async () => {
             axiosMock.mockReturnValueOnce({ data: [{ foo: 'bar', baz: 'abc' }] })
+            expect(await getLatestVersion('/some/url')).toBeUndefined()
+
+            axiosMock.mockReturnValueOnce({ data: 10 })
             expect(await getLatestVersion('/some/url')).toBeUndefined()
         })
 
