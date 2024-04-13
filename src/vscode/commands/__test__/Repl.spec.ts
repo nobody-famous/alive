bd3c17822abdbbd9b7f92d8feb672f7d1229819f
Rich Heller <rellehr@gmail.com>
2024-04-13 16:21:33 -0500

    inlineEval tests

--- src/vscode/commands/__test__/Repl.spec.ts
+++ src/vscode/commands/__test__/Repl.spec.ts
@@ -1,3 +1,5 @@
+import { EvalInfo } from '../../Types'
+import { LSP } from '../../backend/LSP'
 import { clearRepl, inlineEval, sendToRepl } from '../Repl'
 
 const vscodeMock = jest.requireMock('vscode')
@@ -14,15 +16,15 @@ describe('Repl tests', () => {
         expect(ui.clearRepl).toHaveBeenCalled()
     })
 
-    describe('sendToRepl', () => {
-        const fakeEditor = {
-            document: {
-                getText: jest.fn(),
-                uri: { toString: jest.fn() },
-                selection: {},
-            },
-        }
+    const createFakeEditor = () => ({
+        document: {
+            getText: jest.fn(),
+            uri: { toString: jest.fn() },
+            selection: {},
+        },
+    })
 
+    describe('sendToRepl', () => {
         const runTest = async (evalInfo: unknown, validate: () => void) => {
             const lsp = { getEvalInfo: jest.fn().mockReturnValue(evalInfo), evalWithOutput: jest.fn() }
             let editorFn: ((editor: unknown) => Promise<void>) | undefined
@@ -32,7 +34,7 @@ describe('Repl tests', () => {
             })
 
             await sendToRepl(lsp)
-            await editorFn?.(fakeEditor)
+            await editorFn?.(createFakeEditor())
 
             validate()
         }
@@ -47,11 +49,49 @@ describe('Repl tests', () => {
     })
 
     describe('inlineEval', () => {
-        it('', async () => {
-            const lsp = { getEvalInfo: jest.fn(), eval: jest.fn() }
+        beforeEach(() => {
+            vscodeMock.window.showTextDocument.mockReset()
+            vscodeMock.commands.executeCommand.mockReset()
+        })
+
+        const runTest = async (
+            evalInfo: EvalInfo | undefined,
+            evalResult: string | undefined,
+            validate: (lsp: Pick<LSP, 'getEvalInfo' | 'eval'>) => void
+        ) => {
+            const lsp = { getEvalInfo: jest.fn(async () => evalInfo), eval: jest.fn(async () => evalResult) }
             const state = { hoverText: '' }
+            let editorFn: ((editor: unknown) => Promise<void>) | undefined
+
+            utilsMock.useEditor.mockImplementationOnce((langs: string[], fn: (editor: unknown) => Promise<void>) => {
+                editorFn = fn
+            })
 
             await inlineEval(lsp, state)
+            await editorFn?.(createFakeEditor())
+
+            validate(lsp)
+        }
+
+        it('OK', async () => {
+            await runTest({ text: 'some text', package: 'some package' }, 'some result', (lsp) => {
+                expect(vscodeMock.window.showTextDocument).toHaveBeenCalled()
+                expect(vscodeMock.commands.executeCommand).toHaveBeenCalledWith('editor.action.showHover')
+            })
+        })
+
+        it('No info', async () => {
+            await runTest(undefined, undefined, (lsp: Pick<LSP, 'getEvalInfo'>) => {
+                expect(lsp.getEvalInfo).toHaveBeenCalled()
+                expect(vscodeMock.window.showTextDocument).not.toHaveBeenCalled()
+            })
+        })
+
+        it('No eval result', async () => {
+            await runTest({ text: 'some text', package: 'some package' }, undefined, (lsp) => {
+                expect(lsp.eval).toHaveBeenCalled()
+                expect(vscodeMock.window.showTextDocument).not.toHaveBeenCalled()
+            })
         })
     })
 })
