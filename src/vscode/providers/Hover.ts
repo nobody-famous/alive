c06ed7a2349777c535f7c8e4741487961531e678
Rich Heller <rellehr@gmail.com>
2024-04-18 10:15:06 -0500

    Hover provider tests

--- src/vscode/providers/Hover.ts
+++ src/vscode/providers/Hover.ts
@@ -2,31 +2,31 @@ import * as vscode from 'vscode'
 import { LSP } from '../backend/LSP'
 import { ExtensionState } from '../Types'
 
-export function getHoverProvider(state: ExtensionState, lsp: LSP): vscode.HoverProvider {
+type HoverState = Pick<ExtensionState, 'hoverText'>
+type HoverLSP = Pick<LSP, 'getHoverText' | 'getSymbol'>
+
+export function getHoverProvider(state: HoverState, lsp: HoverLSP): Provider {
     return new Provider(state, lsp)
 }
 
 class Provider implements vscode.HoverProvider {
-    private state: ExtensionState
-    private lsp: LSP
+    private state: HoverState
+    private lsp: HoverLSP
 
-    constructor(state: ExtensionState, lsp: LSP) {
+    constructor(state: HoverState, lsp: HoverLSP) {
         this.state = state
         this.lsp = lsp
     }
 
-    async provideHover(doc: vscode.TextDocument, pos: vscode.Position): Promise<vscode.Hover> {
+    async provideHover(doc: Pick<vscode.TextDocument, 'uri'>, pos: vscode.Position): Promise<vscode.Hover> {
         if (this.state.hoverText !== '') {
-            return new vscode.Hover(this.state.hoverText)
+            return new vscode.Hover(new vscode.MarkdownString(this.state.hoverText))
         }
 
         let text = await this.lsp.getHoverText(doc.uri.toString(), pos)
 
         if (text !== '') {
-            const symbol = await this.lsp.getSymbol(doc.uri.toString(), pos)
-            const json = JSON.stringify(symbol)
-
-            text += `<br>[Inspect](command:alive.inspect?${encodeURIComponent(json)})`
+            text += await this.getSymbolLink(doc.uri.toString(), pos)
         }
 
         const mdString = new vscode.MarkdownString(text)
@@ -35,5 +35,16 @@ class Provider implements vscode.HoverProvider {
         mdString.isTrusted = true
 
         return new vscode.Hover(mdString)
+    }
+
+    async getSymbolLink(uri: string, pos: vscode.Position): Promise<string> {
+        const symbol = await this.lsp.getSymbol(uri, pos)
+        if (symbol === undefined) {
+            return ''
+        }
+
+        const json = JSON.stringify(symbol)
+
+        return `<br>[Inspect](command:alive.inspect?${encodeURIComponent(json)})`
     }
 }
