c2f755ad48928bbf52d5c6e5975d1696fd9aa34f
Rich Heller <rellehr@gmail.com>
2024-04-27 17:20:46 -0500

    Initial ReplHistory tests

--- src/vscode/views/__test__/ReplHistory.spec.ts
+++ src/vscode/views/__test__/ReplHistory.spec.ts
@@ -1,3 +1,4 @@
+import { HistoryItem } from '../../Types'
 import { HistoryNode, ReplHistoryTreeProvider, isHistoryNode } from '../ReplHistory'
 
 describe('ReplHistory tests', () => {
@@ -10,5 +11,37 @@ describe('ReplHistory tests', () => {
         const history = new ReplHistoryTreeProvider([])
 
         history.update([{ pkgName: 'foo', text: 'foo' }])
+
+        const kids = history.getChildren()
+
+        expect(Array.isArray(kids)).toBe(true)
+
+        if (Array.isArray(kids)) {
+            expect(kids[0].label).toBe('foo')
+        }
+    })
+
+    it('getItems', () => {
+        const items: HistoryItem[] = []
+        const history = new ReplHistoryTreeProvider(items)
+
+        expect(history.getItems()).toBe(items)
+    })
+
+    it('getCurrentItem', () => {
+        const history = new ReplHistoryTreeProvider([])
+
+        expect(history.getCurrentItem()).toBeUndefined()
+    })
+
+    it('getChildren', () => {
+        const history = new ReplHistoryTreeProvider([])
+        const kids = history.getChildren(new HistoryNode({ pkgName: 'pkg', text: 'text' }))
+
+        expect(Array.isArray(kids)).toBe(true)
+
+        if (Array.isArray(kids)) {
+            expect(kids[0].label).toBe('pkg')
+        }
     })
 })
